8830c0d0b92afff4ad6057bdd75b9d3275102720
OshadhiDilthara <2020cs158@stu.ucsc.cmb.ac.lk>
2023-07-25 20:26:27 +0530

    add card details done

--- lib/screens/pages/profile_page.dart
+++ lib/screens/pages/profile_page.dart
@@ -1,7 +1,19 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:profile/profile.dart';
-import 'package:flutter_credit_card/flutter_credit_card.dart';
+
 import 'package:group_list_view/group_list_view.dart';
+import 'package:flutter_credit_card_new/constants.dart';
+import 'package:flutter_credit_card_new/credit_card_animation.dart';
+import 'package:flutter_credit_card_new/credit_card_background.dart';
+import 'package:flutter_credit_card_new/credit_card_brand.dart';
+import 'package:flutter_credit_card_new/credit_card_form.dart';
+import 'package:flutter_credit_card_new/credit_card_model.dart';
+import 'package:flutter_credit_card_new/credit_card_widget.dart';
+import 'package:flutter_credit_card_new/custom_card_type_icon.dart';
+import 'package:flutter_credit_card_new/flutter_credit_card.dart';
+import 'package:flutter_credit_card_new/glassmorphism_config.dart';
+import 'package:flutter_credit_card_new/localized_text_model.dart';
 
 
 class ProfilePage extends StatefulWidget {
@@ -33,19 +45,21 @@ class _ProfilePageState extends State<ProfilePage> {
                 ),
               ),
             ),
+
             Align(
               alignment: Alignment.topRight,
-              child: ElevatedButton(
+              child: ElevatedButton.icon(
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => CardsPage()), // Navigate to CardsPage
+                    MaterialPageRoute(builder: (context) => EditProfile()),
                   );
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.yellow[700], // Set the button color to yellow
+                  backgroundColor: Colors.yellow[700],
                 ),
-                child: const Text('Card Details'),
+                icon: Icon(Icons.edit), // Edit icon
+                label: Text('Edit Profile'),
               ),
             ),
 
@@ -60,23 +74,26 @@ class _ProfilePageState extends State<ProfilePage> {
               phone_number: '0175773607',
             ),
 
-            const SizedBox(height: 20), // Add some spacing between the details
+            const SizedBox(height: 20),
             Align(
               alignment: Alignment.center,
               child: ElevatedButton(
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => EditProfile()), // Navigate to CardsPage
+                    MaterialPageRoute(builder: (context) => CardsPage()), // Navigate to CardsPage
                   );
-                  // Handle button press
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.yellow[700], // Set the button color to yellow
                 ),
-                child: const Text('Edit Profile'),
+                child: const Text('Payments'),
               ),
             ),
+
+
+
+
             const SizedBox(height: 50),
             Align(
               alignment: Alignment.bottomCenter,
@@ -85,6 +102,10 @@ class _ProfilePageState extends State<ProfilePage> {
                 children: [
                   ElevatedButton(
                     onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => BecomeCatcher()), // Navigate to CardsPage
+                      );
                       // Handle button press
                     },
                     style: ElevatedButton.styleFrom(
@@ -111,6 +132,20 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 }
 
+class BecomeCatcher extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.red[50],
+        appBar: AppBar(
+        title: const Text('Become a Catcher'),
+        ),
+    );
+
+  }
+
+}
+
 
 
 
@@ -122,11 +157,12 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  final String _profilePictureUrl = ''; // Store the profile picture URL here
+  final String _profilePictureUrl = 'https://images.unsplash.com/photo-1598618356794-eb1720430eb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'; // Store the profile picture URL here
 
   // Method to handle saving profile changes
   void _saveChanges() {
@@ -155,10 +191,11 @@ class _EditProfileState extends State<EditProfile> {
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           children: [
             const SizedBox(height: 16.0),
-            // Profile Picture
-            const CircleAvatar(
-              radius: 60.0,
 
+            // Profile Picture
+             CircleAvatar(
+              radius: 60.0,
+              backgroundImage: NetworkImage(_profilePictureUrl),
 
 
             ),
@@ -168,7 +205,7 @@ class _EditProfileState extends State<EditProfile> {
                 // Implement the logic to change profile picture here
                 // You can use image picker packages to choose an image from the gallery or camera
                 // For simplicity, we'll assume the URL of the new profile picture is stored in _profilePictureUrl
-                // _profilePictureUrl = ... (update the URL here)
+
                 setState(() {});
               },
               child: const Text('Change Profile Picture'),
@@ -260,18 +297,251 @@ class CardsPage extends StatelessWidget {
   }
 }
 
-class AddNewCard extends StatelessWidget {
+class AddNewCard extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red[50],
-      appBar: AppBar(
-        title: const Text('Add New Card'),
-      ),
-      body: const Center(
-        child: Text('Add New Card Page'),
+  State<StatefulWidget> createState() {
+    return AddNewCardState();
+  }
+}
+
+class AddNewCardState extends State<AddNewCard> {
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool isCvvFocused = false;
+  bool useGlassMorphism = false;
+  bool useBackgroundImage = false;
+  OutlineInputBorder? border;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.grey.withOpacity(0.7),
+        width: 2.0,
       ),
     );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      title: 'Flutter Credit Card View Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.red[50],
+        appBar: AppBar(
+
+          title: const Text('Add New Card'),
+          backgroundColor: Colors.green,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context); // This will navigate back to the previous screen
+            },
+          ),
+        ),
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          decoration: BoxDecoration(
+            image: !useBackgroundImage
+                ? const DecorationImage(
+              image: ExactAssetImage('assets/bg.png'),
+              fit: BoxFit.fill,
+            )
+                : null,
+
+          ),
+          child: SafeArea(
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 30,
+                ),
+                CreditCardWidget(
+                  glassmorphismConfig:
+                  useGlassMorphism ? Glassmorphism.defaultConfig() : null,
+                  cardNumber: cardNumber,
+                  expiryDate: expiryDate,
+                  cardHolderName: cardHolderName,
+                  cvvCode: cvvCode,
+                  showBackView: isCvvFocused,
+                  obscureCardNumber: true,
+                  obscureCardCvv: true,
+                  isHolderNameVisible: true,
+                  cardBgColor: Colors.red,
+                  backgroundImage:
+                  useBackgroundImage ? 'assets/images/card_bg.jpg' : null,
+                  isSwipeGestureEnabled: true,
+                  onCreditCardWidgetChange:
+                      (CreditCardBrand creditCardBrand) {},
+                  customCardTypeIcons: <CustomCardTypeIcon>[
+                    CustomCardTypeIcon(
+                      cardType: CardType.mastercard,
+                      cardImage: Image.asset(
+                        'assets/images/mastercard.png',
+                        height: 48,
+                        width: 48,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        CreditCardForm(
+                          formKey: formKey,
+                          obscureCvv: true,
+                          obscureNumber: true,
+                          cardNumber: cardNumber,
+                          cvvCode: cvvCode,
+                          isHolderNameVisible: true,
+                          isCardNumberVisible: true,
+                          isExpiryDateVisible: true,
+                          cardHolderName: cardHolderName,
+                          expiryDate: expiryDate,
+                          themeColor: Colors.blue,
+                          cardNumberDecoration: InputDecoration(
+                            labelText: 'Number',
+                            hintText: 'XXXX XXXX XXXX XXXX',
+                            hintStyle: const TextStyle(color: Colors.black),
+                            labelStyle: const TextStyle(color: Colors.black),
+                            focusedBorder: border,
+                            enabledBorder: border,
+                          ),
+                          expiryDateDecoration: InputDecoration(
+                            hintStyle: const TextStyle(color: Colors.black),
+                            labelStyle: const TextStyle(color: Colors.black),
+                            focusedBorder: border,
+                            enabledBorder: border,
+                            labelText: 'Expired Date',
+                            hintText: 'XX/XX',
+                          ),
+                          cvvCodeDecoration: InputDecoration(
+                            hintStyle: const TextStyle(color: Colors.black),
+                            labelStyle: const TextStyle(color: Colors.black),
+                            focusedBorder: border,
+                            enabledBorder: border,
+                            labelText: 'CVV',
+                            hintText: 'XXX',
+                          ),
+                          cardHolderDecoration: InputDecoration(
+                            hintStyle: const TextStyle(color: Colors.black),
+                            labelStyle: const TextStyle(color: Colors.black),
+                            focusedBorder: border,
+                            enabledBorder: border,
+                            labelText: 'Card Holder',
+                          ),
+                          onCreditCardModelChange: onCreditCardModelChange,
+                          textStyle:
+                          const TextStyle(color: Colors.black, fontSize: 12),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const Text(
+                              'Glassmorphism',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Switch(
+                              value: useGlassMorphism,
+                              inactiveTrackColor: Colors.grey,
+                              activeColor: Colors.white,
+                              activeTrackColor: Colors.green,
+                              onChanged: (bool value) => setState(() {
+                                useGlassMorphism = value;
+                              }),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const Text(
+                              'Card Image',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Switch(
+                              value: useBackgroundImage,
+                              inactiveTrackColor: Colors.grey,
+                              activeColor: Colors.white,
+                              activeTrackColor: Colors.green,
+                              onChanged: (bool value) => setState(() {
+                                useBackgroundImage = value;
+                              }),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            primary: const Color(0xff1b447b),
+                          ),
+                          child: Container(
+                            margin: const EdgeInsets.all(12),
+                            child: const Text(
+                              'Validate',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'halter',
+                                fontSize: 14,
+                                package: 'flutter_credit_card',
+                              ),
+                            ),
+                          ),
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              print('valid!');
+                            } else {
+                              print('invalid!');
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onCreditCardModelChange(CreditCardModel? creditCardModel) {
+    setState(() {
+      cardNumber = creditCardModel!.cardNumber;
+      expiryDate = creditCardModel.expiryDate;
+      cardHolderName = creditCardModel.cardHolderName;
+      cvvCode = creditCardModel.cvvCode;
+      isCvvFocused = creditCardModel.isCvvFocused;
+    });
   }
 }
 
